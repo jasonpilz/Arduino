0b13cf8bca909c50609445545713b97d92031203
Jason Pilz <jasonpilz@gmail.com>
2015-12-06 08:02:14 -0700

    Implement logging of temp readings to sparkfun data service

--- sense_temperature.rb
+++ sense_temperature.rb
@@ -1,18 +1,25 @@
 require 'arduino_firmata'
+require 'net/http'
 
 arduino = ArduinoFirmata.connect
 
-names = %w(Ella Ada Emery bobba dahdah Hunna)
+public_key = ENV['SF_TEMP_PUBLIC_KEY']
+private_key = ENV['SF_TEMP_PRIVATE_KEY']
 
 loop do
-  temp = arduino.analog_read(1) * 100 * 5 / 1024
-  puts temp
+  temp = sprintf "%.2f", arduino.analog_read(1) * (100 * 5 / 1024.0)
+  time = Time.now.strftime("%I:%M%p")
 
-  if temp < 60
-    system "say", "Temperature reading is #{temp}...are you cold #{names.shuffle.sample}?"
+  uri = URI("http://data.sparkfun.com/input/#{public_key}?private_key=#{private_key}&temp=#{temp}")
+  response = Net::HTTP.get(uri)
+
+  if response.include?("success")
+    puts "Temperature reading of #{temp} degrees uploaded to data stream at #{time}"
   else
-    system "say", "Temperature reading is #{temp} farenheit"
+    puts "Error uploading temp reading"
   end
 
-  sleep(10)
+  sleep(60 * 30)
 end
+
+# system "say", "Temperature reading is #{temp} farenheit"
